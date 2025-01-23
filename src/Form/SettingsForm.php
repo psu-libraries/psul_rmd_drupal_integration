e275e1eb442628d46147da97db12306ad5f9fbaa
Mike Henninger <msh6004@psu.edu>
2025-01-23 09:44:47 -0500

    Issue #3 - Adding cache_ttl to settings and settings form.

--- src/Form/SettingsForm.php
+++ src/Form/SettingsForm.php
@@ -35,12 +35,22 @@ final class SettingsForm extends ConfigFormBase {
       '#title' => $this->t('API URL'),
       '#default_value' => $this->config('psul_rmd_drupal_integration.settings')->get('api_url') ?? 'https://metadata.libraries.psu.edu/v1/',
       '#required' => TRUE,
+      '#description' => $this->t('URL to the API endpoint. This should end with a slash.  The API documentation can be found at <a href="https://metadata.libraries.psu.edu/api_docs">https://metadata.libraries.psu.edu/api_docs</a>.'),
     ];
 
     $form['api_key'] = [
       '#type' => 'textfield',
       '#title' => $this->t('API Key'),
       '#default_value' => $this->config('psul_rmd_drupal_integration.settings')->get('api_key') ?? '',
+      '#description' => $this->t('API key to access the API. This is currently not required to pull profile data.'),
+    ];
+
+    $form['cache_ttl'] = [
+      '#type' => 'number',
+      '#title' => $this->t('Cache TTL'),
+      '#default_value' => $this->config('psul_rmd_drupal_integration.settings')->get('cache_ttl') ?? 172800,
+      '#required' => TRUE,
+      '#description' => $this->t('Time in seconds to cache data from the API. Default is 172800 seconds (2 days).'),
     ];
 
     return parent::buildForm($form, $form_state);
@@ -64,6 +74,7 @@ final class SettingsForm extends ConfigFormBase {
     $this->config('psul_rmd_drupal_integration.settings')
       ->set('api_url', $form_state->getValue('api_url'))
       ->set('api_key', $form_state->getValue('api_key'))
+      ->set('cache_ttl', $form_state->getValue('cache_ttl'))
       ->save();
     parent::submitForm($form, $form_state);
   }
